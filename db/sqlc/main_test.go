780f358c4e92bdf4c007203117ee70997a4af8a5
Borys Yermokhin <borys.yermokhin@roadsurfer.com>
2024-03-24 21:19:30 +0100

    introduce viper usage

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -3,24 +3,23 @@ package db
 import (
 	"database/sql"
 	"log"
+	"master_class/db/util"
 	"os"
 	"testing"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://golang:golang@localhost:5432/simple_bank?sslmode=disable"
-)
-
 var testQueries *Queries
 var testDb *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
-	testDb, err = sql.Open(dbDriver, dbSource)
+	testDb, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
